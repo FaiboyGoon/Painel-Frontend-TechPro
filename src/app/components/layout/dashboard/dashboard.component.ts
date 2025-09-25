612a17317d0bdc144391ce1ad12f7e6f427a7e49
Fabricio Quintana <fabriciorcq@gmail.com>
2025-09-25 18:44:14 -0300

    - Update 9/25/2025 6:43 PM
    - Implementando Dashboard

--- src/app/components/layout/dashboard/dashboard.component.ts
+++ src/app/components/layout/dashboard/dashboard.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { DashboardService } from '../../../services/dashboard.service';
+import Swal from 'sweetalert2';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, MdbFormsModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  dashboardData: any = {};
+  anoAtual: number = new Date().getFullYear();
+  mesAtual: number = new Date().getMonth() + 1;
+
+  dashboardService = inject(DashboardService);
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.carregarDashboardAtual();
+  }
+
+  carregarDashboardAtual(): void {
+    this.dashboardService.obterDashboardAtual().subscribe({
+      next: (data) => (this.dashboardData = data),
+      error: (e) =>
+        Swal.fire({
+          title: 'Erro ao carregar a dashboard atual',
+          text: e.error,
+          icon: 'error',
+          confirmButtonText: 'Ok',
+        }),
+    });
+  }
+
+  carregarDashboardMes(ano: number, mes: number): void {
+    this.dashboardService.obterDashboardMes(ano, mes).subscribe({
+      next: (data) => (this.dashboardData = data),
+      error: (e) =>
+        Swal.fire({
+          title: 'Erro ao carregar a dashboard do mês',
+          text: e.error,
+          icon: 'error',
+          confirmButtonText: 'Ok',
+        }),
+    });
+  }
+
+  carregarDashboardAno(ano: number): void {
+    this.dashboardService.obterDashboardAno(ano).subscribe({
+      next: (data) => (this.dashboardData = data),
+      error: (e) =>
+        Swal.fire({
+          title: 'Erro ao carregar a dashboard do ano',
+          text: e.error,
+          icon: 'error',
+          confirmButtonText: 'Ok',
+        }),
+    });
+  }
+
+  getKeys(obj: any): string[] {
+  return obj ? Object.keys(obj) : [];
+}
 
 }
