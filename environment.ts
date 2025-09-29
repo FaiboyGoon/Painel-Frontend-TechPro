e23b9c9dc2b06d0e1f64fd8c3e983debc83ca146
João Pedro Quadro Carvalho <jpgnoatto15@gmail.com>
2025-09-29 13:17:26 -0300

    Create environment.ts

---
+++ environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiBase: 'https://api.exemplo.com', // <-- COLE AQUI A BASE API 
+  exchangePath: '/exchange',          // caminho opcional para câmbio (ajuste se necessário)
+  transactionsPath: '/transactions'   // caminho opcional para extrato (ajuste se necessário)
+};
